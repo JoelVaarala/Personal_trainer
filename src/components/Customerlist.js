21da1fb6ad872ddcb3487567fe54998372410579
JoelVaarala <bgh275@myy.haaga-helia.fi>
2020-04-20 19:42:04 +0300

    Customer deleting

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect} from 'react';
 import ReactTable from 'react-table-v6'
 import 'react-table-v6/react-table.css'
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 
 export default function Customerlist(){
 
 const[customers, setCustomers] = useState([]);
+const[msg, SetMsg] = useState('');
+const[open, setOpen] = useState(false);
 
 useEffect(() => {
   getCustomers();
@@ -16,6 +21,22 @@ const getCustomers = () => {
     .then(response => response.json())
     .then(res => setCustomers(res.content))
     .catch(err => console.log(err))
+}
+
+const deleteCustomer = (link) => {
+  if(window.confirm('Would you like to delete this customer?')){
+    fetch(link, {method: 'DELETE'})
+    .then(_ => getCustomers())
+    .then(_ => {
+      SetMsg('Customer deleted.');
+      setOpen(true);
+    })
+    .catch(err => console.log(err))
+  }
+}
+
+const handleClose = () => {
+  setOpen(false);
 }
 
 const columns = [
@@ -47,6 +68,9 @@ const columns = [
     Header: 'City',
     accessor: 'city'
   },
+  {
+    Cell: row => (<Button  color="secondary" startIcon={<DeleteIcon />}  onClick={() => deleteCustomer(row.original.links[0].href)}></Button>)
+  },
 ] 
 
 
@@ -55,6 +79,16 @@ return (
       <ReactTable data={customers} defaultPageSize={20}
         columns={columns} filterable={true}/>
 
+      <Snackbar
+            open={open}
+            autoHideDuration={3000}
+            onClose={handleClose}
+            message={msg}
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left'
+            }}
+      />
     </div>
   );
 }
